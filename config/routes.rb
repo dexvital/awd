7c2d4588486edd7993dc9605a1d442bb272267b5
vitalijs.romanovskis <vitalijs.romanovskis@visma.com>
2019-08-01 13:44:37 +0300

    cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
+  root 'store#index', as: 'store_index'
+
   resources :products
+
   get 'say/hello'
   get 'say/goodbye'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
