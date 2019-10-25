71f19f6ad4a56def7e04974daee4205ece7ca6f5
vitalijs.romanovskis <vitalijs.romanovskis@visma.com>
2019-10-25 16:45:22 +0300

    Cart use Ajax to add items

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  fixtures :
+  fixtures :products
 
   # test "the truth" do
   #   assert true
@@ -16,9 +16,9 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   test "product price must be positive" do
-    product = Product.new(title: "My Book Title",
-                          description: "yyy",
-                          image_url: "zzz.jpg")
+    product = Product.new(title: 'My Book Title',
+                          description: 'yyy',
+                          image_url: 'zzz.jpg')
     product.price = -1
     assert product.invalid?
     assert_equal ["must be greater than or equal to 0.01"],
@@ -32,8 +32,8 @@ class ProductTest < ActiveSupport::TestCase
   end
 
   def new_product(image_url)
-    Product.new(title: "My Book Title",
-                description: "yyy",
+    Product.new(title: 'My Book Title',
+                description: 'yyy',
                 price: 1,
                 image_url: image_url)
   end
@@ -51,9 +51,9 @@ class ProductTest < ActiveSupport::TestCase
 
   test "product is not valid without a unique title" do
     product = Product.new(title: products(:ruby).title,
-                          description: "yyy",
+                          description: 'yyy',
                           price: 1,
-                          image_url: "fred.gif")
+                          image_url: 'fred.gif')
     assert product.invalid?
     assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
   end
