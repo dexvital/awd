002fdc2b602a0f6f2029e7abe7665ec85b104fac
vitalijs.romanovskis <vitalijs.romanovskis@visma.com>
2019-11-21 16:19:07 +0200

    Ability to use RSS

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   resources :carts
   root 'store#index', as: 'store_index'
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   get 'say/hello'
   get 'say/goodbye'
